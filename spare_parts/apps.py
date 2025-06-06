3eaf51c9d74367c0c7b8c650ff7ac68ef4ceac85
rmbproduction <repairmybike81@gmail.com>
2025-06-06 02:14:55 -0700

    .\spare_parts\apps.py

---
+++ spare_parts/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SparePartsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'spare_parts'
+    verbose_name = 'Spare Parts' 
